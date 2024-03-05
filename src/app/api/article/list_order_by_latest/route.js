fb79ba6e6f61bf9fc14e91891ea532ee3441e052
cagdaskarabulut <cagdas.karabulut@gmail.com>
2024-03-05 14:48:42 +0300

    .

--- src/app/api/article/list_order_by_latest/route.js
+++
@@ -1,8 +0,0 @@
-import { sql } from "@vercel/postgres";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const article_list =
-    await sql`SELECT id, url, title, topics, create_date, like_number, title_image, body, is_manuel_page, description, meta_keys, view_number FROM public.arabulucu_message order by create_date desc;`;
-  return NextResponse.json({ article_list });
-}
