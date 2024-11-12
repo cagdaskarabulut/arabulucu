7a6024e0c07d1f53fa2103c3bc5212f5c8742c80
Çağdaş Yıldırım Karabulut <cagdas@Cagdas-MacBook-Pro.local>
2024-11-12 20:50:18 +0300

    .

--- src/pages/iletisim.js
+++ src/pages/iletisim.js
@@ -5,15 +5,14 @@ import React from "react";
 
 //-Ucret-Hesaplama sayfası
 export default function iletisim() {
-
   return (
     <>
       <MetaPanel
         title="Avukat Arabulucu Öznur İlhan Karabulut - İletişim"
         descriptionContent="Avukat Arabulucu Öznur İlhan Karabulut ile iletişime geçmek için bu sayfayı kullanın. Deneyimli hukukçu ve arabulucu olan Öznur İlhan Karabulut'a hukuki sorunlarınızı iletebilirsiniz. İhtiyacınıza yönelik hukuki destek almak için bize ulaşın."
-        keywordsContent="Avukat, Arabulucu, İletişim, Hukuki Destek, İletişim Bilgileri, Hukuk Danışmanlığı,Arabuluculuk Danışmanlığı, Uzman Arabulucu, Hukuk Danışman, Hukuki İletişim, Hukukçu"
+        keywordsContent="İstanbul Arabulucu Avukat,Avukat, Arabulucu, İletişim, Hukuki Destek, İletişim Bilgileri, Hukuk Danışmanlığı,Arabuluculuk Danışmanlığı, Uzman Arabulucu, Hukuk Danışman, Hukuki İletişim, Hukukçu"
       />
-      <PageTemplate content={<MessageForm/>} />
+      <PageTemplate content={<MessageForm />} />
     </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <MetaPanel
-        title="Arabuluculuk nedir? - Avukat Arabulucu Öznur İlhan Karabulut - Arabuluculuk ve Hukuk Danışmanlığı - İstanbul Arabulucu"
+        title="Arabuluculuk nedir? - Avukat Arabulucu Öznur İlhan Karabulut - Arabuluculuk ve Hukuk Danışmanlığı - İstanbul Arabulucu Avukat"
         descriptionContent="Avukat Arabulucu Öznur İlhan Karabulut, pek çok dava konusunda deneyime sahip bir avukat ve arabulucudur. Arabuluculuk ve hukuk danışmanlığı alanında uzmanlık sunar. İhtiyaçlarınıza özel çözümler için bize başvurun ve hukuki sorunlarınızı çözümleyin."
         keywordsContent="istanbul arabulucu, Uzman Arabulucu, İş Hukuku Arabulucusu, Kira Hukuku Arabulucusu, İş Hukuku, Kira Hukuku, Ortaklığın Giderilmesi, Mal Paylaşımı, Ortaklığın Giderilmesi Arabuluculuğu, Avukat Arabulucu, Öznur İlhan Karabulut, Arabuluculuk, Hukuk Danışmanlığı, Hukuki Uzmanlık, Hukuki Destek, Uyuşmazlık Çözümü, Deneyimli Hukukçu, ıstanbul arabulucu"
         imagePath="/images/Arabulucu.jpg"
